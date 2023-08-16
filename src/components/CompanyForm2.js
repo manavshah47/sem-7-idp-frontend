02d2ad07e511b4362123f8e29d58e5e8a2f3ab18
kunal070 <kp058665@gmail.com>
2023-08-16 22:44:49 +0530

    2 form validation

--- src/components/CompanyForm2.js
+++ src/components/CompanyForm2.js
@@ -24,6 +24,8 @@ const CompanyForm = ({session}) => {
     });
 
     const navigate = useNavigate();
+    const [errors, setErrors] = useState({});
+
 
     const navigatepreviouspage = () => {
         navigate('/membership-form');
@@ -56,7 +58,55 @@ const CompanyForm = ({session}) => {
         preLoadData()
     }, [])
 
-
+    const validateForm = () => {
+        let newErrors = {};
+    
+        if (!formData.companyType.trim()) {
+            newErrors.companyType = 'Company Type is required';
+        }
+    
+        if (!formData.registrationYear.trim()) {
+            newErrors.registrationYear = 'Registration Date is required';
+        }
+    
+        if (!formData.panNumber.trim()) {
+            newErrors.panNumber = 'PAN Number is required';
+        } 
+        // else if (!/^([A-Z]){5}([0-9]){4}([A-Z]){1}$/.test(formData.panNumber)) {
+        //     newErrors.panNumber = 'Invalid PAN number format.';
+        // }
+    
+        if (!formData.cinNumber.trim()) {
+            newErrors.cinNumber = 'CIN Number is required';
+        } 
+        // else if (!/^([A-Z]){1}([0-9]){5}([A-Z]){2}([0-9]){4}([A-Z]){3}([0-9]){6}$/.test(formData.cinNumber)) {
+        //     newErrors.cinNumber = 'Invalid CIN number format.';
+        // }
+    
+        if (!formData.registrationProofName.trim()) {
+            newErrors.registrationProofName = 'Registration Proof Name is required';
+        }
+    
+        if (!formData.gstNumber.trim()) {
+            newErrors.gstNumber = 'GST Number is required';
+        } 
+        // else if (!/^\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d[Z]{1}[A-Z\d]{1}$/.test(formData.gstNumber)) {
+        //     newErrors.gstNumber = 'Invalid GST number format.';
+        // }
+        
+    
+        if (!formData.registrationProof) {
+            newErrors.registrationProof = 'Registration Proof is required';
+        } 
+        // else if (formData.registrationProof.type !== 'application/pdf') {
+        //     newErrors.registrationProof = 'File must be in PDF format.';
+        // }
+        
+    
+        setErrors(newErrors);
+        return newErrors;
+    };
+    
     const handleChange = (e) => {
         setIsDataUpdated(true)
         const { name, value, type } = e.target;
@@ -78,18 +128,29 @@ const CompanyForm = ({session}) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        console.log('handleSubmit triggered');
+
 
         if(!isDataUpdated){
+            console.log('Navigating to /company-info-2');
             navigate("/company-info-3")
             return;
         }
 
-        const response = await axios.put(`${process.env.REACT_APP_BASE_URL}/membership/company-info-2`, {...formData}, {headers:{"Content-Type":"multipart/form-data"}})
-        toast(response.data.message)
+        const isValid = validateForm();
+        if (isValid) {
+            // withCredentials to send httpOnly cookie via request
+            axios.defaults.withCredentials = true;
 
-        if(response.data.success){
-            navigate("/company-info-3")
-        }
+            const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/membership/company-info-2`, {...formData}, {headers:{"Content-Type":"application/json"}})
+
+            if(response.data.success){
+                toast(response.data.message)
+                navigate('/company-info-3');
+            } else {
+                toast(response.data.message)
+            }
+        }        
     };
 
     return (
@@ -104,14 +165,16 @@ const CompanyForm = ({session}) => {
                     </div>
                     <div className='width-50' style={{marginLeft:'10px'}}>
                         <input type="text" name="companyType" value={formData.companyType} onChange={handleChange} required style={{ backgroundColor: '#eee' }} />   
+                        {errors.companyType && <p className="error-message">{errors.companyType}</p>}
                     </div>
                 </div>
                 <div className="form-group flex width-50" >
                     <div className='width-50' style={{marginLeft:'45px'}}>
                         <p className='label' style={{textAlign:'start'}}>Registration Date:</p>
                     </div>
-                    <div className='width-50' style={{marginLeft:'10px'}}>
+                    <div className='width-50' style={{marginLeft:'0px'}}>
                         <input type="date" name="registrationYear" value={formData.registrationYear} onChange={handleChange} required style={{ backgroundColor: '#eee' }} />
+                        {errors.registrationYear && <p className="error-message">{errors.registrationYear}</p>}
                     </div>
                 </div>
             </div>
@@ -124,6 +187,7 @@ const CompanyForm = ({session}) => {
                     </div>
                     <div className='width-50' style={{marginLeft:'10px'}}>
                         <input type="text" name="panNumber" value={formData.panNumber} onChange={handleChange} required style={{ backgroundColor: '#eee' }} />   
+                        {errors.panNumber && <p className="error-message">{errors.panNumber}</p>}
                     </div>
                 </div>
                 <div className="form-group flex width-50" >
@@ -132,26 +196,29 @@ const CompanyForm = ({session}) => {
                     </div>
                     <div className='width-50' style={{marginLeft:'10px'}}>
                         <input type="text" name="cinNumber" value={formData.cinNumber} onChange={handleChange} style={{ backgroundColor: '#eee' }} />
+                        {errors.cinNumber && <p className="error-message">{errors.cinNumber}</p>}
                     </div>
                 </div>
             </div>
             <div style={{ display: 'flex', flexDirection: 'row' ,paddingLeft:'20px', paddingRight:'20px'}}>
                 <div className="form-group flex width-50" >
-                    <div className='width-50' style={{marginLeft:'45px'}}>
+                    <div className='width-50' style={{marginLeft:'50px'}}>
                         <p className='label' style={{textAlign:'start'}}>Registration Proof Name:</p>
                     </div>
                     <div className='width-50' style={{marginLeft:'10px'}}>
                         <input style={{backgroundColor:'#eee'}} type="text" name="registrationProofName" value={formData.registrationProofName} onChange={handleChange} required />
+                        {errors.registrationProofName && <p className="error-message">{errors.registrationProofName}</p>}
                     </div>
                 </div>
 
                 <div className="form-group flex width-50" >
-                    <div className='width-50' style={{marginLeft:'50px'}}>
+                    <div className='width-50' style={{marginLeft:'45px'}}>
                         <p className='label' style={{textAlign:'start'}}>GST Number:</p>
                     </div>
                     <div className='width-50' style={{marginLeft:'10px'}}>
                         <input type="text" name="gstNumber" value={formData.gstNumber} onChange={handleChange} required style={{ backgroundColor: '#eee' }} />
-                       </div>
+                        {errors.gstNumber && <p className="error-message">{errors.gstNumber}</p>}
+                    </div>
                 </div>
                 
             </div>
@@ -160,8 +227,9 @@ const CompanyForm = ({session}) => {
                     <div className='width-50'>
                         <p className='label'  style={{ marginRight: 20, textAlign:'start'}}>Registration Proof:</p>
                     </div>
-                    <div className='width-50' style={{marginLeft:'10px'}}>
+                    <div className='width-50' style={{marginLeft:'0px'}}>
                         <input type="file" name="file" onChange={handleChange} accept=".pdf" required style={{ backgroundColor: '#eee' }} />       
+                        {errors.registrationProof && <p className="error-message">{errors.registrationProof}</p>}
                     </div>
                 </div>
             </div>
